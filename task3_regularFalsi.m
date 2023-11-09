26aa75eb020e9ebf4c48cdd61bec5d2f348606f4
Nivrita44 <107574615+Nivrita44@users.noreply.github.com>
2023-11-10 00:04:27 +0600

    Update task3_regularFalsi.m

--- task3_regularFalsi.m
+++ task3_regularFalsi.m
@@ -1,9 +1,10 @@
   function retval = task3_regularFalsi(input1, input2)
-  f = input('Enter your function ');
+  f = @(x) x.^2 - 4;
 
-  a = input('Enter left side of interval ');
-  b = input('Enter right side of interval ');
+  a = 0;
+  b = 6;
   tol = 1e-6;
+  M=[];
 
 
   iterations = 0;
@@ -13,21 +14,22 @@
 
  while f(a) * f(b) < 0 && a<b
 
-     x = b-((b-a)/(f(b)-f(a)))*f(b);
-     if abs(f(x))<tol
+     c= b-((b-a)/(f(b)-f(a)))*f(b);
+     M=[M;c];
+     if abs(f(c))<tol
        break
      end
-     if f(a)*f(x) <0
-       b=x;
+     if f(a)*f(c) <0
+       b=c;
      else
-       a=x;
+       a=c;
      end
      iterations = iterations + 1;
-      x_vals = [x_vals, iterations];
-      y_vals = [y_vals, x];
-      root = b-((b-a)/(f(b)-f(a)))*f(b);
+      %x_vals = [x_vals, iterations];
+     % y_vals = [y_vals, x];
 
- end
+end
+  root = M(end);
 
   fprintf('Root found after %d iterations: %f\n', iterations, root);
   fprintf('y_vals = [');
@@ -35,7 +37,20 @@
     fprintf('%f\n', y_vals(i));
   end
   fprintf(']\n');
+  x=linspace(1,2,50);
+  y=f(x);
+  figure;
+  plot (x, y, 'LineWidth',2);
+  hold on;
+  scatter(M, f(M), 40, 'r', 'filled');
+  xlabel('x');
+  ylabel('f(x)');
+  title('False-Position Method: Finding Roots');
+  legend('Function', 'Roots');
+  grid on;
+  hold off;
 
 
- endfunction
+  retval = root;
+endfunction
 
